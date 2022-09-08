c54e53d986189271e73e4e495eece103c7ad40f8
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-07 21:46:58 -0300

    feat: Main List styles 90% done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Main(){
-    const empty = true;
+    const empty = false;
     const emptyText = "There are no income or expense logs yet..."
-    const list = []
+    const navigate = useNavigate();
+    const list = [
+        { value: -1233, description: "Almoco", date:"11/30" },
+        { value: 1000, description: "Salario", date:"11/27"},
+
+        { value: -1233, description: "Churrasco", date:"11/26"},
+        { value: 1000, description: "Salario", date:"11/20"},
+
+        { value: -1233, description: "Mercado", date:"11/19"},
+        { value: 1000, description: "Salario", date:"11/19"},
+
+        { value: -1233, description: "Papinha", date:"11/18"},
+        { value: 1000, description: "Salario", date:"11/15"},
+
+    ]
     function returnToMain(){
         console.log('bolinha');
     }
@@ -15,14 +30,47 @@ export default function Main(){
                 <div onClick={returnToMain}><ion-icon name="exit-outline"></ion-icon></div>
             </MainTitle>
 
-            {empty?<EmptyBox><div>{emptyText}</div></EmptyBox>:<></>}
+            {
+                empty?
+
+                <EmptyBox><div>{emptyText}</div></EmptyBox>:
+
+                <MainBox>{list.map((item,index)=>{
+                    return (<Item value={item.value} date={item.date} description={item.description} key={index}/>);
+                })}</MainBox>
+            }
+
             <ButtonWrapper>
-                <NewButton><div><ion-icon name="add-circle-outline"></ion-icon></div><div>New Income</div></NewButton>
-                <NewButton><div><ion-icon name="remove-circle-outline"></ion-icon></div><div>New Expense</div></NewButton>
+                <NewButton onClick={()=>navigate("/income")}><div><ion-icon name="add-circle-outline"></ion-icon></div><div>New Income</div></NewButton>
+                <NewButton onClick={()=>navigate("/expense")}><div><ion-icon name="remove-circle-outline"></ion-icon></div><div>New Expense</div></NewButton>
             </ButtonWrapper>
         </MainWrapper>
     );
 }
+
+function Item({value,date,description}){
+    console.log(value);
+    return(
+        <Entry>
+            <Date><p>{date}</p>{description}</Date>{value}
+        </Entry>
+    );
+
+}
+
+const Date = styled.div`
+    p{
+        color:grey;
+    }
+    display: flex;
+`;
+const Entry = styled.div`
+    display:flex;
+    justify-content: space-between;
+    font-size: 2.5vh;
+    width: 90%;
+    margin: 1vh 0;
+`;
 
 const MainWrapper = styled.div`
     width: 90vw;
@@ -52,16 +100,18 @@ const Box = styled.div`
     flex-direction: column;
     background-color: white;
     width: 100%;
-    height: 80%;
+    height: 100%;
     border-radius:6px
 `;
 
 const MainBox = styled(Box)`
-    justify-content: center;
-    background-color:blue;
+    overflow: scroll;
+    align-items: center;
+    background-color:rgba(255,255,255,1);
 `;
 
 const EmptyBox = styled(Box)`
+
     align-items: center;
     justify-content: center;
     color: rgba(134, 134, 134, 1);
